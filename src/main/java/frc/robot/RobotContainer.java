0a5bd777f15171e09e509d339976e53b23159e76
swaswa999 <106714567+swaswa999@users.noreply.github.com>
2026-02-12 17:23:47 -0800

    disable intake pivot

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -17,7 +17,7 @@ import frc.robot.subsystems.swerve.SwerveSubsystem;
 // import frc.robot.subsystems.Vision.VisionSubsystem;
 // import frc.robot.subsystems.Vision.CameraConfig;
 import frc.robot.subsystems.Intake.RollerIntakeSubsystem;
-import frc.robot.subsystems.Intake.PivotIntakeSubsystem;
+//import frc.robot.subsystems.Intake.PivotIntakeSubsystem;
 import frc.robot.subsystems.hopper.HopperSubsystem;
 // import frc.robot.Constants.IntakeConstants;
 
@@ -64,7 +64,7 @@ public class RobotContainer {
   private SwerveSubsystem swerveSubsystem = Constants.SWERVE_ENABLED ? new SwerveSubsystem(swerveCAN) : null;
 
   private final RollerIntakeSubsystem intakeSubsystem = new RollerIntakeSubsystem(mechCAN);
-  private final PivotIntakeSubsystem pivotIntake = new PivotIntakeSubsystem(mechCAN);
+  //private final PivotIntakeSubsystem pivotIntake = new PivotIntakeSubsystem(mechCAN);
   private final HopperSubsystem HopperSubsystem = new HopperSubsystem(mechCAN);
   private final Field2d m_field = new Field2d();
   private final ClimbSubsystem m_ClimbSubsystem = new ClimbSubsystem(mechCAN);
@@ -171,7 +171,7 @@ public class RobotContainer {
 
     // ==================== INTAKE PIVOT ====================
     // Right stick Y controls pivot manually
-    pivotIntake.setDefaultCommand(Commands.run(() -> {
+    /*pivotIntake.setDefaultCommand(Commands.run(() -> {
       double pivotInput = -mechController.getRightY();
       if (Math.abs(pivotInput) > 0.1) {
         pivotIntake.setManualSpeed(pivotInput * 0.3);
@@ -179,6 +179,7 @@ public class RobotContainer {
         pivotIntake.stop();
       }
     }, pivotIntake));
+    */
     
 
     // ==================== HOPPER ====================
