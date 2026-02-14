47451e1bd0a4f9ae2fbeacf28fc25744669d9e5f
Vineet Petlur <141369334+FractalGeometryMathematician@users.noreply.github.com>
2026-02-14 09:55:01 -0800

    Refactor angle and velocity calculation methods

--- src/main/java/frc/robot/subsystems/shooter/kinemat.java
+++ src/main/java/frc/robot/subsystems/shooter/kinemat.java
@@ -4,11 +4,11 @@ import frc.robot.Constants.railgunConstants;
 public class kinemat {
     
     public static double calculateAngle(double dista){
-        return Math.toDegrees(Math.atan( (6/(dista))*(1+ Math.sqrt(1- (1.8288/3)) ) ));
+        return Math.atan((2*3.0/D)*(1 + Math.sqrt(1 - 1.828/3.0)));
     }
 
     public static double calculateVel(double ang){
-        return Math.sqrt(6*9.8/ ((Math.toDegrees(Math.sin(Math.toRadians(ang))))*(Math.toDegrees(Math.sin(Math.toRadians(ang))))) );
+        return Math.sqrt(2*9.8*3.0) / Math.sin(theta);  
     }
 
     public static double rotationSpeed(double linearVel){
