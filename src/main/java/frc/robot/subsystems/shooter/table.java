c16c86a90437a75ede9c3cf3b7d8ac33bb910387
swaswa999 <106714567+swaswa999@users.noreply.github.com>
2026-02-20 17:00:33 -0800

    hashmapInterpolation Table

--- src/main/java/frc/robot/subsystems/shooter/table.java
+++ src/main/java/frc/robot/subsystems/shooter/table.java
@@ -3,7 +3,14 @@ package frc.robot.subsystems.shooter;
 import java.util.HashMap;
 import java.util.Map;
 
+
 public class table {
     
+    Map<Double, int arr[2]> idk = new HashMap<>();
+
+    public lookup(double dist){
+        
+    }
+
 
 }
