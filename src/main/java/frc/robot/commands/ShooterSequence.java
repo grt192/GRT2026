d9ceaf8dd6fa871321ef80cc74e2e9c71de549ea
Vineet Petlur <141369334+FractalGeometryMathematician@users.noreply.github.com>
2026-02-24 14:17:29 -0800

    Modify ShooterSequence to include Tower command
    Added Tower parameter to ShooterSequence constructor and included RunTower command in the sequence.

--- src/main/java/frc/robot/commands/ShooterSequence.java
+++ src/main/java/frc/robot/commands/ShooterSequence.java
@@ -1,6 +1,12 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.shooter.flywheel;
+import frc.robot.subsystems.shooter.hood;
+import frc.robot.subsystems.shooter.flywheel;
+
 public class ShooterSequence extends SequentialCommandGroup {
     public ShooterSequence(Drive drive, Shooter shooter,
-                           Hood hood, Indexer indexer, Vision vision) {
+                           Hood hood, Indexer indexer, Vision vision, Tower tower) {
 
         addCommands(
             new AimCommand(drive, vision)
@@ -12,6 +18,7 @@ public class ShooterSequence extends SequentialCommandGroup {
                 new SetHoodAngle(hood),
                 new ConditionalCommand(
                     new RunIndexer(indexer),
+                    new RunTower(tower);
                     Commands.none(),
                     () -> shooter.atSpeed() && hood.atSetpoint()
                 )
