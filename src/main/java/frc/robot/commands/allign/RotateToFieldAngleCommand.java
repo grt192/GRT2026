ab7461b2b6ae26bf38534bbc0188f875a1582e44
swaswa999 <swayams@icloud.com>
2026-02-28 14:39:08 -0800

    I love claude code it generated this, i also don't know what this is but i hope its right

--- src/main/java/frc/robot/commands/allign/RotateToFieldAngleCommand.java
+++ src/main/java/frc/robot/commands/allign/RotateToFieldAngleCommand.java
@@ -3,7 +3,9 @@ package frc.robot.commands.allign;
 import java.util.function.BooleanSupplier;
 
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.RotateToAngleConstants;
 import frc.robot.subsystems.swerve.SwerveSubsystem;
@@ -14,10 +16,22 @@ public class RotateToFieldAngleCommand extends Command {
     private final double targetDegrees;
     private final BooleanSupplier cancelCondition;
 
+    private static final NetworkTable table = NetworkTableInstance.getDefault().getTable("AutoAim");
+    private static final NetworkTableEntry kPEntry = table.getEntry("kP");
+    private static final NetworkTableEntry kIEntry = table.getEntry("kI");
+    private static final NetworkTableEntry kDEntry = table.getEntry("kD");
+    private static final NetworkTableEntry toleranceEntry = table.getEntry("Tolerance");
+    private static final NetworkTableEntry goalEntry = table.getEntry("Goal");
+    private static final NetworkTableEntry actualEntry = table.getEntry("Actual");
+    private static final NetworkTableEntry errorEntry = table.getEntry("Error");
+    private static final NetworkTableEntry outputEntry = table.getEntry("Output");
+
+    private static boolean initialized = false;
+
     public RotateToFieldAngleCommand(SwerveSubsystem swerve, double targetDegrees, BooleanSupplier cancelCondition) {
         this.swerve = swerve;
         this.targetDegrees = targetDegrees;
-        
+
         this.cancelCondition = cancelCondition;
         this.pid = new PIDController(
             RotateToAngleConstants.kP,
@@ -28,39 +42,45 @@ public class RotateToFieldAngleCommand extends Command {
         pid.setTolerance(RotateToAngleConstants.TOLERANCE_DEGREES);
         addRequirements(swerve);
 
-        // Publish initial PID values to NetworkTables so you can edit them in Shuffleboard/SmartDashboard
-        SmartDashboard.putNumber("RotateToAngle/kP", RotateToAngleConstants.kP);
-        SmartDashboard.putNumber("RotateToAngle/kI", RotateToAngleConstants.kI);
-        SmartDashboard.putNumber("RotateToAngle/kD", RotateToAngleConstants.kD);
-        SmartDashboard.putNumber("RotateToAngle/Tolerance", RotateToAngleConstants.TOLERANCE_DEGREES);
+        if (!initialized) {
+            kPEntry.setDouble(RotateToAngleConstants.kP);
+            kIEntry.setDouble(RotateToAngleConstants.kI);
+            kDEntry.setDouble(RotateToAngleConstants.kD);
+            toleranceEntry.setDouble(RotateToAngleConstants.TOLERANCE_DEGREES);
+            initialized = true;
+        }
     }
 
     @Override
     public void initialize() {
-        // Read PID values from NetworkTables each time the command starts
-        pid.setP(SmartDashboard.getNumber("RotateToAngle/kP", RotateToAngleConstants.kP));
-        pid.setI(SmartDashboard.getNumber("RotateToAngle/kI", RotateToAngleConstants.kI));
-        pid.setD(SmartDashboard.getNumber("RotateToAngle/kD", RotateToAngleConstants.kD));
-        pid.setTolerance(SmartDashboard.getNumber("RotateToAngle/Tolerance", RotateToAngleConstants.TOLERANCE_DEGREES));
+        pid.setP(kPEntry.getDouble(RotateToAngleConstants.kP));
+        pid.setI(kIEntry.getDouble(RotateToAngleConstants.kI));
+        pid.setD(kDEntry.getDouble(RotateToAngleConstants.kD));
+        pid.setTolerance(toleranceEntry.getDouble(RotateToAngleConstants.TOLERANCE_DEGREES));
         pid.reset();
     }
 
     @Override
     public void execute() {
-        // Use field-absolute rotation from pose, not driver heading
+        // Live update PID values from NetworkTables
+        pid.setP(kPEntry.getDouble(RotateToAngleConstants.kP));
+        pid.setI(kIEntry.getDouble(RotateToAngleConstants.kI));
+        pid.setD(kDEntry.getDouble(RotateToAngleConstants.kD));
+        pid.setTolerance(toleranceEntry.getDouble(RotateToAngleConstants.TOLERANCE_DEGREES));
+
         double currentAngle = normalizeAngle(swerve.getRobotPosition().getRotation().getDegrees());
         double normalizedTarget = normalizeAngle(targetDegrees);
         double rotationPower = pid.calculate(currentAngle, normalizedTarget);
-        // Don't negate rotation power - Pigeon negation in pose estimator already accounts for direction
         swerve.setDrivePowers(0, 0, rotationPower);
 
-        SmartDashboard.putNumber("RotateToAngle/Goal", normalizedTarget);
-        SmartDashboard.putNumber("RotateToAngle/Actual", currentAngle);
-        // Calculate error with wrap-around
+        // Publish feedback to NetworkTables
+        goalEntry.setDouble(normalizedTarget);
+        actualEntry.setDouble(currentAngle);
         double error = normalizedTarget - currentAngle;
         if (error > 180) error -= 360;
         if (error < -180) error += 360;
-        SmartDashboard.putNumber("RotateToAngle/Error", error);
+        errorEntry.setDouble(error);
+        outputEntry.setDouble(rotationPower);
     }
 
     /**
