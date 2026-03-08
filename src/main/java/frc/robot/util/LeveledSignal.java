42e704bc49e0b3babc9c0cb5d89e42a09b2161d7
littleStinker <72678136+LittleStinkerGuy@users.noreply.github.com>
2026-03-07 23:32:03 -0800

    add LeveledSignal

---
+++ src/main/java/frc/robot/util/LeveledSignal.java
@@ -0,0 +1,42 @@
+package frc.robot.util;
+
+import java.util.function.Consumer;
+
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.Timestamp;
+
+import frc.robot.util.LoggedTalon.TelemetryLevel;
+
+public class LeveledSignal<T> {
+    private final StatusSignal<T> signal;
+    public final TelemetryLevel level;
+    public Consumer<String> logger;
+
+    public LeveledSignal(StatusSignal<T> signal, TelemetryLevel level, Consumer<String> logger) {
+        this.signal = signal;
+        this.level = level;
+        this.logger = logger;
+    }
+
+    public T getValue() {
+        return signal.getValue();
+    }
+
+    public Timestamp getTimestamp() {
+        return signal.getTimestamp();
+    }
+
+    public StatusSignal<T> refresh() {
+        return signal.refresh();
+    }
+
+    public StatusCode getStatus() {
+        return signal.getStatus();
+    }
+
+    public BaseStatusSignal getBaseSignal() {
+        return signal;
+    }
+}
