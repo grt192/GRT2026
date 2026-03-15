fc0feabd6f313c64a2e7e63ff2e7e86a7ae3a059
swaswa999 <106714567+swaswa999@users.noreply.github.com>
2026-03-14 17:07:08 -0700

    add pivot out  befor

--- src/main/java/frc/robot/commands/auton/ShootAndLeaveAuton.java
+++ src/main/java/frc/robot/commands/auton/ShootAndLeaveAuton.java
@@ -10,12 +10,14 @@ import frc.robot.Constants.TowerConstants;
 import frc.robot.Constants.TowerConstants.TOWER_INTAKE;
 import frc.robot.commands.allign.AimToHubCommand;
 import frc.robot.commands.hopper.indexerRun;
+import frc.robot.commands.intake.pivot.PivotOutCommand;
 import frc.robot.commands.shooter.SpinFlywheelCommand;
 import frc.robot.commands.shooter.hoodCommand;
 import frc.robot.commands.shooter.rampFlywheel;
 import frc.robot.commands.shooter.towerRollers.towerRoll;
 import frc.robot.commands.swerve.DriveBackwardsCommand;
 import frc.robot.subsystems.FMS.FieldManagementSubsystem;
+import frc.robot.subsystems.Intake.PivotIntakeSubsystem;
 import frc.robot.subsystems.hopper.HopperSubsystem;
 import frc.robot.subsystems.swerve.SwerveSubsystem;
 import frc.robot.subsystems.shooter.flywheel;
@@ -28,25 +30,28 @@ public class ShootAndLeaveAuton extends SequentialCommandGroup {
     private HopperSubsystem hopperSubsystem;
     private towerRollers towerSubsystem;
     private hood hoodSubsystem;
+    private PivotIntakeSubsystem pivotIntakeSubsystem;
 
     // Link to dimensions https://firstfrc.blob.core.windows.net/frc2026/FieldAssets/2026-field-dimension-dwgs.pdf
-    public ShootAndLeaveAuton(SwerveSubsystem swerveSubsystem, flywheel flySubsystem, hood hoodSubsystem, HopperSubsystem hopperSubsystem, towerRollers towerSubsystem) {
+    public ShootAndLeaveAuton(SwerveSubsystem swerveSubsystem, flywheel flySubsystem, hood hoodSubsystem, HopperSubsystem hopperSubsystem, towerRollers towerSubsystem, PivotIntakeSubsystem pivotIntakeSubsystem) {
         this.swerveSubsystem = swerveSubsystem;
         this.flySubsystem = flySubsystem;
         this.hoodSubsystem = hoodSubsystem;
         this.hopperSubsystem = hopperSubsystem;
         this.towerSubsystem = towerSubsystem;
+        this.pivotIntakeSubsystem = pivotIntakeSubsystem;
 
         addCommands(
+            new PivotOutCommand(pivotIntakeSubsystem),
             new SpinFlywheelCommand(flySubsystem, .4)
-            .alongWith(
-                Commands.runOnce(() -> hoodSubsystem.setHoodAngle(0), hoodSubsystem),
-                Commands.waitSeconds(5)
-                .andThen(Commands.run(() -> hopperSubsystem.setHopper(HOPPER_INTAKE.BALLIN), hopperSubsystem)
-                .alongWith(Commands.run(()->towerSubsystem.setTower(TOWER_INTAKE.BALLUP), towerSubsystem)))
-            // .withTimeout(10)
-            // .andThen(new DriveBackwardsCommand(swerveSubsystem, AlignConstants.AUTON_SPEEDS)
-            ));
+                .alongWith(
+                    Commands.runOnce(() -> hoodSubsystem.setHoodAngle(0), hoodSubsystem),
+                    Commands.waitSeconds(5)
+                        .andThen(Commands.run(() -> hopperSubsystem.setHopper(HOPPER_INTAKE.BALLIN), hopperSubsystem)
+                            .alongWith(Commands.run(() -> towerSubsystem.setTower(TOWER_INTAKE.BALLUP), towerSubsystem)))
+                // .withTimeout(10)
+                // .andThen(new DriveBackwardsCommand(swerveSubsystem, AlignConstants.AUTON_SPEEDS)
+                ));
 
     }
 }
