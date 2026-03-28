9decb19f8ec16a5ec40c84514f9d7ef793bf7e8d
littleStinker <72678136+LittleStinkerGuy@users.noreply.github.com>
2026-03-28 04:10:19 -0700

    configure candle

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -1,13 +1,43 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.configs.CANdleConfiguration;
+import com.ctre.phoenix6.configs.CANdleFeaturesConfigs;
+import com.ctre.phoenix6.configs.LEDConfigs;
+import com.ctre.phoenix6.controls.ColorFlowAnimation;
+import com.ctre.phoenix6.controls.LarsonAnimation;
+import com.ctre.phoenix6.controls.SingleFadeAnimation;
+import com.ctre.phoenix6.controls.SolidColor;
+import com.ctre.phoenix6.controls.StrobeAnimation;
 import com.ctre.phoenix6.hardware.CANdle;
+import com.ctre.phoenix6.signals.LarsonBounceValue;
+import com.ctre.phoenix6.signals.LossOfSignalBehaviorValue;
+import com.ctre.phoenix6.signals.RGBWColor;
+import com.ctre.phoenix6.signals.StatusLedWhenActiveValue;
+import com.ctre.phoenix6.signals.StripTypeValue;
+import edu.wpi.first.units.measure.Frequency;
+import edu.wpi.first.units.measure.Time;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LEDSubsystem extends SubsystemBase {
     private CANdle candle;
+    private CANdleConfiguration candleConfig;
 
     public LEDSubsystem(CANBus can) {
         candle = new CANdle(0, can);
+
+        configureCANdle();
+    }
+
+    private void configureCANdle() {
+        candleConfig = new CANdleConfiguration()
+            .withLED(new LEDConfigs()
+                .withLossOfSignalBehavior(LossOfSignalBehaviorValue.DisableLEDs)
+                .withStripType(StripTypeValue.RGB))
+            .withCANdleFeatures(new CANdleFeaturesConfigs()
+                .withStatusLedWhenActive(StatusLedWhenActiveValue.Disabled));
+
+        candle.getConfigurator().apply(candleConfig);
+    }
     }
 }
