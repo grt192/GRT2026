a06063740842d522a4a33bd095d350d1702a122e
littleStinker <72678136+LittleStinkerGuy@users.noreply.github.com>
2026-04-03 10:53:51 -0700

    set flywheel rps for cycle shooter to 80

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -417,7 +417,7 @@ public final class Constants {
         public static final double HOOD_POSITION = 0.096;
 
         // Flywheel speed (RPS)
-        public static final double FLYWHEEL_RPS = 96;
+        public static final double FLYWHEEL_RPS = 80;
 
         // Tower duty cycle
         public static final double TOWER_DUTY_CYCLE = 0.7;
