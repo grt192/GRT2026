e47173fb021a2ec9643f943b8899701fdb2da67c
littleStinker <72678136+LittleStinkerGuy@users.noreply.github.com>
2026-04-03 14:38:57 -0700

    remove auton init form robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -115,7 +115,6 @@ public class Robot extends LoggedRobot {
 
     @Override
     public void autonomousInit() {
-        m_robotContainer.onAutonInit();
         m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
         // schedule the autonomous command
