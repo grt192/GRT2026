acc6f136835956d85bf907c0786e957c6450b866
Luna Wang <zlunawang@gmail.com>
2026-04-09 17:02:04 -0700

    intake

---
+++ src/main/java/frc/robot/commands/intake/PivotAndRollerIntakeCommand.java
@@ -0,0 +1,43 @@
+package frc.robot.commands.intake;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.IntakeConstants;
+import frc.robot.subsystems.Intake.PivotIntakeSubsystem;
+import frc.robot.subsystems.Intake.RollerIntakeSubsystem;
+
+
+public class PivotAndRollerIntakeCommand extends Command {
+    private final PivotIntakeSubsystem pivotIntake;
+    private final RollerIntakeSubsystem rollerIntake;
+
+    public PivotAndRollerIntakeCommand(
+        PivotIntakeSubsystem pivotIntake,
+        RollerIntakeSubsystem rollerIntake) {
+        this.pivotIntake = pivotIntake;
+        this.rollerIntake = rollerIntake;
+        addRequirements(pivotIntake, rollerIntake);
+    }
+
+    @Override
+    public void initialize() {
+        pivotIntake.setPosition(IntakeConstants.PIVOT_OUT_POS);
+        rollerIntake.runIn();
+    }
+
+    @Override
+    public void execute() {
+        pivotIntake.setPosition(IntakeConstants.PIVOT_OUT_POS);
+        rollerIntake.runIn();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        pivotIntake.setPosition(IntakeConstants.PIVOT_IN_POS);
+        rollerIntake.stop();
+    }
+}
