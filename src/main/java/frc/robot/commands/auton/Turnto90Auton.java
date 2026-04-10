3574177b444b01f2b00f5a3968d0de64e61c2dd4
Luna Wang <zlunawang@gmail.com>
2026-04-10 00:48:20 -0700

    turn 90 deg auton!

---
+++ src/main/java/frc/robot/commands/auton/Turnto90Auton.java
@@ -0,0 +1,17 @@
+package frc.robot.commands.auton;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.allign.RotateToFieldAngleCommand;
+import frc.robot.subsystems.swerve.SwerveSubsystem;
+
+public class Turnto90Auton extends SequentialCommandGroup {
+
+    public Turnto90Auton(SwerveSubsystem swerveSubsystem) {
+        double currentAngle = swerveSubsystem.getRobotPosition().getRotation().getDegrees();
+        double targetAngle = currentAngle + 90;
+
+        addCommands(
+            new RotateToFieldAngleCommand(swerveSubsystem, targetAngle, () -> false)
+        );
+    }
+}
